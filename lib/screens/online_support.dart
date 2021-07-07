ff2515d4d89de7124f7faabfed96f21d92d6e41e
Phn Truong <51257908+phntrg121@users.noreply.github.com>
2021-07-08 00:19:13 +0700

    payment, support

--- lib/screens/online_support.dart
+++ lib/screens/online_support.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gtnm/models/message_model.dart';
+import 'package:gtnm/widgets/message_item.dart';
 
 class OnlineSupportScreen extends StatefulWidget {
   OnlineSupportScreen({Key? key}) : super(key: key);
@@ -8,10 +10,26 @@ class OnlineSupportScreen extends StatefulWidget {
 }
 
 class _OnlineSupportScreenState extends State<OnlineSupportScreen>{
+  List<MessageModel> _message = [];
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
+    _message .add(MessageModel(
+      content: 'Xin chào, chúng tôi có thể giúp gì cho bạn?', 
+      from: 'supporter'));
   }
+
+  void _send(){
+    setState(() {
+      _message .add(MessageModel(
+      content: _textEditingController.text, 
+      from: 'user'));
+    });
+    _textEditingController.clear();
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +40,18 @@ class _OnlineSupportScreenState extends State<OnlineSupportScreen>{
       body: Column(
         children: [
           Expanded(
-            child: Center(),
+            child: ListView(
+              children: _message.map((mes) {
+                return MessageItem(model: mes);
+              }).toList(),
+            )
           ),
           Container(
             padding: EdgeInsets.all(10),
             child: Row(
               children: [
                 Flexible(child: TextField(
+                  controller: _textEditingController,
                   decoration: InputDecoration(
                     hintText: "Yêu cầu tư vấn ...",
                     hintStyle: TextStyle(
@@ -37,7 +60,7 @@ class _OnlineSupportScreenState extends State<OnlineSupportScreen>{
                     ),
                   ),
                 )),
-                IconButton(onPressed: (){}, icon: Icon(Icons.send)),
+                IconButton(onPressed: _send, icon: Icon(Icons.send)),
               ],
             ),
           )
---
+++ lib/widgets/pay_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:gtnm/models/product_model.dart';
+import 'package:gtnm/screens/detail.dart';
+
+class PayItem extends StatelessWidget{
+  PayItem({required this.model});
+  ProductModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Card(
+        child: Row(
+            children: [
+              Image(
+                image: NetworkImage(model.image),
+                width: 100,
+                height: 100,
+              ),
+              Column(
+                children: [
+                  Text(model.ten),
+                  Text(model.gia.toString()),
+                ],
+              ),
+            ],
+        ),
+      ),
+      onTap: (){},
+    );
+  }
+}
